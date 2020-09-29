c39109917ec51ad179b27a443752f31388d03a15
mehakjit <mehakjitsingh@gmail.com>
2020-09-29 13:15:50 +0530

    [Mehakjit] Created: Valid First Name Entry

--- src/com/capg/UserRegisteration.java
+++ src/com/capg/UserRegisteration.java
@@ -1,8 +1,28 @@
 package com.capg;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegisteration {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to User Registration Wizard");
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Enter First name ");
+		String firstName = sc.nextLine();
+		Pattern pattern =Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+		Matcher matcher = pattern.matcher(firstName);
+		boolean matchfound = matcher.find();
+		
+		if(matchfound) {
+			System.out.println("Match found");
+		}
+		else {
+			System.out.println("Incorrect name " + "\n" + "Correct name should start with Capital letter and should have min 3 charcatersall in small.");
+		}		
+	
+		sc.close();
 	}
 }
